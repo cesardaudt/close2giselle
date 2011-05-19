5c2294bc742ca09df9e96254b7d6e2ac701c1a7f
garciadaudt <garciadaudt@116232e7-3993-1f59-6fb9-5f7724841252>
2011-05-19 18:33:11 +0000

    Deleted some unused commented code

--- trunk/camera.cpp
+++ trunk/camera.cpp
@@ -28,7 +28,8 @@ void Camera::set(float max[2], float min[2], int w, int h, vector3f translate, v
 	look_from.x = look_at.x = (max[0] + min[0])/2; 
 	look_from.y = look_at.y = (max[1] + min[1])/2;
 	
-	//rewrite this part
+	//TODO: Should work if fovy xor fovx is given
+	//		Should be clearer
 	hy = ((max[1] - min[1])/2)/tan(DEG_RAD(fovy/2.0));
 
 	ph = (h/2)/tan(DEG_RAD(fovy/2));
@@ -47,21 +48,12 @@ void Camera::set(float max[2], float min[2], int w, int h, vector3f translate, v
     glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
 
-//	look_from.x += translate.x;
-//	look_from.y += translate.y;
-//	look_from.z += translate.z;
 	look_from += translate;	
 		
 	if(!lookat) {
-//		look_at.x += translate.x;
-//		look_at.y += translate.y;
-//		look_at.z += translate.z;
 		look_at += translate;
 	}
 
-//	n.x = -look_from.x + look_at.x;
-//	n.y = -look_from.y + look_at.y;
-//	n.z = look_at.z;
 	n = look_at - look_from;
 	
 	n.normalize();
@@ -76,51 +68,19 @@ void Camera::set(float max[2], float min[2], int w, int h, vector3f translate, v
 	//rotation in x
 	matrix4x4f rx;
 	rx.rotate(rotation.x, u);
-	
-//	vector3f aux_n;
-//	aux_n.x = n.x; aux_n.y = n.y; aux_n.z = n.z;
-//	rx.transformVector(&aux_n);
-//	n.x = aux_n.x; n.y = aux_n.y; n.z = aux_n.z;
 	rx.transformVector(&n);
-
-	
-//	vector3f aux_v;
-//	aux_v.x = v.x; aux_v.y = v.y; aux_v.z = v.z;
-//	rx.transformVector(&aux_v);
-//	v.x = aux_v.x; v.y = aux_v.y; v.z = aux_v.z;
 	rx.transformVector(&v);
 		
 	//rotation in y
 	matrix4x4f ry;
 	ry.rotate(rotation.y, v);
-	
-//	vector3f aux_n;
-//	aux_n.x = n.x; aux_n.y = n.y; aux_n.z = n.z;
-//	ry.transformVector(&aux_n);
-//	n.x = aux_n.x; n.y = aux_n.y; n.z = aux_n.z;
 	ry.transformVector(&n);
-
-	
-//	vector3f aux_u;
-//	aux_u.x = u.x; aux_u.y = u.y; aux_u.z = u.z;
-//	ry.transformVector(&aux_u);
-//	u.x = aux_u.x; u.y = aux_u.y; u.z = aux_u.z;
 	ry.transformVector(&u);
 
 	//rotation in z
 	matrix4x4f rz;
 	rz.rotate(rotation.z, n);
-
-//	vector3f aux_v;
-//	aux_v.x = v.x; aux_v.y = v.y; aux_v.z = v.z;
-//	rz.transformVector(&aux_v);	
-//	v.x = aux_v.x; v.y = aux_v.y; v.z = aux_v.z;
 	rz.transformVector(&v);
-	
-//	vector3f aux_u;
-//	aux_u.x = u.x; aux_u.y = u.y; aux_u.z = u.z;
-//	rz.transformVector(&aux_u);
-//	u.x = aux_u.x; u.y = aux_u.y; u.z = aux_u.z;
 	rz.transformVector(&u);
 
     gluLookAt(look_from.x      , look_from.y      , look_from.z      ,
